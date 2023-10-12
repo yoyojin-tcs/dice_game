836436a83c3383880ff39ba1dc85f4d969e41b08
BITniki <shakarimovdima@ya.ru>
2023-10-12 20:59:18 +0300

    remove singleton pattern

--- src/com/yoyojin/edu/dice_game/idGenerator/IncrementIdGeneratorStrategy.java
+++ src/com/yoyojin/edu/dice_game/idGenerator/IncrementIdGeneratorStrategy.java
@@ -1,19 +1,13 @@
 package com.yoyojin.edu.dice_game.idGenerator;
 
 public class IncrementIdGeneratorStrategy implements IdGenerator {
-    private static IncrementIdGeneratorStrategy INSTANCE;
     private Long currentId;
 
-
-    private IncrementIdGeneratorStrategy(Long currentId) {
-        this.currentId = currentId;
+    public IncrementIdGeneratorStrategy() {
     }
 
-    public static IncrementIdGeneratorStrategy getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new IncrementIdGeneratorStrategy(0L);
-        }
-        return INSTANCE;
+    public IncrementIdGeneratorStrategy(Long initId) {
+        this.currentId = initId;
     }
 
     @Override
